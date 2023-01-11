c13d5c8157879366c1dad79ca7984843514b32f3
Ivan Bulyk <stackyogi@gmail.com>
2023-01-11 15:14:53 +0100

    fixed review comments in 1_2_box_pin_homework-1.0

--- 1_concepts/1_2_box_pin/src/lib.rs
+++ 1_concepts/1_2_box_pin/src/lib.rs
@@ -7,16 +7,17 @@ trait MutMeSomehow {
     fn mut_me_somehow(self: std::pin::Pin<&mut Self>);
 }
 
-
-impl<T: std::fmt::Display> MutMeSomehow for Box<T> {
-    fn mut_me_somehow(self: std::pin::Pin<&mut Self>) {
-        self.get_mut().to_string();
+impl<T: std::clone::Clone> MutMeSomehow for Box<T> {
+    fn mut_me_somehow(mut self: std::pin::Pin<&mut Self>) {
+      let mut a =  self.clone();
+      self.set(a);
     }
 }
 
 impl<T: std::fmt::Display> MutMeSomehow for Rc<T> {
-    fn mut_me_somehow(self: std::pin::Pin<&mut Self>) {
-        self.get_mut().clone();
+    fn mut_me_somehow(mut self: std::pin::Pin<&mut Self>) {
+        let mut a = self.clone();
+        self.clone_from(&a);
     }
 }
 
@@ -38,23 +39,74 @@ impl MutMeSomehow for String {
 
 impl MutMeSomehow for &[u8] {
     fn mut_me_somehow(mut self: std::pin::Pin<&mut Self>) {
-        self.get_mut().bytes();
+        let mut x = self.clone();
+        let mut y = self.clone();
+        x.clone_from(&y);
+        self.set(x);
     }
 }
 
 mod t_parent {
+    use std::{fmt, io, pin::Pin, ops::Deref};
+
     trait MutMeSomehow {
         fn mut_me_somehow(self: std::pin::Pin<&mut Self>);
     }
 
-    impl<T,K> MutMeSomehow for (T,K ){
+
+    impl<T: std::clone::Clone> MutMeSomehow for T {
+
         fn mut_me_somehow(mut self: std::pin::Pin<&mut Self>) {
-            self.into_ref();
+
+            let mut d = self.clone();
+            self.set(d);
+        }
+    }
+
+    trait SayHi: fmt::Debug {
+        fn say_hi(self: Pin<&Self>) {
+            println!("Hi from {:?}", self)
+        }
+    }
+
+    impl<T: fmt::Debug> SayHi for T {
+        fn say_hi(self: Pin<&Self>) {
+            println!("Hi from {:?}", self)
         }
     }
 }
 
 trait SayHi: fmt::Debug {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self)
+    }
+}
+
+impl<T: fmt::Debug> SayHi for Box<T> {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self)
+    }
+}
+
+impl<T: fmt::Debug> SayHi for Rc<T> {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self)
+    }
+}
+
+impl<T: fmt::Debug> SayHi for Vec<T> {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self)
+    }
+}
+
+impl SayHi for String {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self)
+    }
+}
+
+impl SayHi for &[u8] {
     fn say_hi(self: Pin<&Self>) {
         println!("Hi from {:?}", self)
     }
