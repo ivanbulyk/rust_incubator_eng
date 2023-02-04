e310b32e2d68dd14fcaf9f21fe7c2ddb8f6eae83
Ivan Bulyk <stackyogi@gmail.com>
2023-02-04 11:53:00 +0100

    fixed review comments in 1_9_phantom homework-1.0

--- 1_concepts/1_9_phantom/src/lib.rs
+++ 1_concepts/1_9_phantom/src/lib.rs
@@ -2,40 +2,74 @@ use rand::prelude::*;
 use std::marker::PhantomData;
 
 pub struct Fact<T> {
-    _marker: PhantomData<*const T>,
+    _marker: PhantomData<T>,
 }
 
-impl<T > Fact<T> {
+impl<T> Fact<T> {
     pub fn new() -> Self {
         Self {
             _marker: PhantomData,
         }
     }
 
-    pub fn fact(&self) -> &'static str {
-        match self._marker {
-            PhantomData::<i32> => {
-                let facts = [
-                    "i32 is a signed 32-bit integer",
-                    "i32 can represent values from -2^31 to 2^31 - 1",
-                    "i32 is a common choice for representing whole numbers",
-                ];
-                let mut rng = rand::thread_rng();
-                let index = rng.gen_range(0..facts.len());
-                &facts[index]
-            }
-            PhantomData::<f64> => {
-                let facts = [
+}
+
+impl Fact<i32> {
+    pub fn fact(&self) -> Option<&str>{
+        let facts = [
+            "i32 is a signed 32-bit integer",
+            "i32 can represent values from -2^31 to 2^31 - 1",
+            "i32 is a common choice for representing whole numbers",
+        ];
+        let mut rng = rand::thread_rng();
+        facts.choose(&mut rng).copied()
+    }
+}
+
+impl Fact<f64> {
+    pub fn fact(&self) -> Option<&str> {
+        let facts = [
                     "f64 is a 64-bit floating-point number",
                     "f64 provides about 15-17 decimal digits of precision",
                     "f64 is the most common choice for representing real numbers",
                 ];
-                let mut rng = rand::thread_rng();
-                let index = rng.gen_range(0..facts.len());
-                &facts[index]
-            }
-            // ... 
-            _ => panic!("Unsupported type"),
-        }
+        let mut rng = rand::thread_rng();
+        facts.choose(&mut rng).copied()
+    }
+}
+
+impl Fact<&'static str> {
+    pub fn fact(&self) -> Option<&str>{
+        let facts = [
+                    "&str is a string slice type",
+                    "&str is immutable",
+                    "&str is often used for text data",
+                ];
+        let mut rng = rand::thread_rng();
+        facts.choose(&mut rng).copied()
+    }
+}
+
+impl<T> Fact<Vec<T>> {
+    pub fn fact(&self) -> Option<&str> {
+        let facts = [
+                    "Vec<T> is a dynamic array type",
+                    "Vec<T> grows as elements are added",
+                    "Vec<T> is often used for collections of values",
+                ];
+        let mut rng = rand::thread_rng();
+        facts.choose(&mut rng).copied()
+    }
+}
+
+impl Fact<u32> {
+    pub fn fact(&self) -> Option<&str> {
+        let facts = [
+                    "u32 is an unsigned 32-bit integer",
+                    "u32 can represent values from 0 to 2^32 - 1",
+                    "u32 is often used for representing non-negative whole numbers",
+                ];
+        let mut rng = rand::thread_rng();
+        facts.choose(&mut rng).copied()
     }
 }
